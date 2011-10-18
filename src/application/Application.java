67b0289b28c1abfb4ce82a949663dc134490bc03
unknown <Brittany Ro@.(none)>
2011-10-18 11:12:10 -0400

    started GUI

---
+++ src/application/Application.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+import userinterface.*;
+
+/**
+ *
+ * @author Brittany Ro
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame.main(args);
+    }
+}
