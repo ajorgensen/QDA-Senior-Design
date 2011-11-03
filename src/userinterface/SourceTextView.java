256103121a090e073e1b4bdfb0e099af7dabd398
nkounkou <brittany.nkounkou@uconn.edu>
2011-11-03 00:35:01 -0400

    progress on the user interface: view containers are working.

---
+++ src/userinterface/SourceTextView.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinterface;
+
+/**
+ *
+ * @author Brittany Nkounkou
+ */
+public class SourceTextView extends View {
+    public SourceTextView(String t) {
+        super(t);
+    }
+}
---
+++ src/userinterface/View.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinterface;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.*;
+
+/**
+ *
+ * @author Brittany Nkounkou
+ */
+public class View extends javax.swing.JScrollPane {
+    private String title;
+    private String abbrv;
+    
+    protected View(String t) {
+        setTitle(t);
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        panel.setMinimumSize(new Dimension(500, 0));
+        panel.setPreferredSize(new Dimension(1000, 100));
+        GroupLayout jPanel1Layout = new GroupLayout(panel);
+        panel.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 197, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 247, Short.MAX_VALUE)
+        );
+        setViewportView(panel);
+    }
+    
+    public final String getTitle() {
+        return title;
+    }
+    
+    public final void setTitle(String t) {
+        title = t;
+        if (t.length() > 25) {
+            abbrv = t.substring(0, 20)+"...  ";
+        }
+        else {
+            abbrv = t+"  ";
+        }
+    }
+    
+    public final String getAbbrv() {
+        return abbrv;
+    }
+}
